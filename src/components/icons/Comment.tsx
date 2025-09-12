772986290a59f13fefd2399eded27e3cf2c3bc27
JulioC090 <juliocziviani@gmail.com>
2025-09-12 17:58:31 -0300

    fix: comment icon

--- src/components/icons/Comment.tsx
+++ src/components/icons/Comment.tsx
@@ -18,13 +18,12 @@ export default function Comment({ className }: CommentProps) {
       viewBox="0 0 24 24"
       width="24"
     >
-      <title>Comentar</title>
       <path
         d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z"
         fill="none"
         stroke="currentColor"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinejoin="round"
+        strokeWidth="2"
       ></path>
     </svg>
   );
